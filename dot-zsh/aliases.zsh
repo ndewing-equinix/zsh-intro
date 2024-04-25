54078bb60934715bb4548aa878e8839a8b31eb6f
Nick Dewing <ndewing@equinix.com>
2024-04-24 23:16:03 -0700

    Adding alias

---
+++ dot-zsh/aliases.zsh
@@ -0,0 +1,16 @@
+# Shortened commands
+alias g='git'
+alias h='history'
+alias gr='grep'
+
+# ls
+alias ls='eza -a --group-directories-first'
+alias ll='ls -l' # inherits from above alias
+
+# Colorize Grep output
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# Override the spinal macro
+alias vim='nvim'
